13a2ca931deec905edab4439acd57bd011cdabff
eshan 33 <dw5000.tw@gmail.com>
2025-10-06 04:47:28 +0800

    Add files via upload

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1759688824|145027417";
+const CACHE_VERSION = "1759696112|1156603613";
 const CACHE_PREFIX = "ChurchInteractiv-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
